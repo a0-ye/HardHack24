15749dc5175a666845d67b9e65cbadf1b29edb47
ayeUCSD <114363008+ayeUCSD@users.noreply.github.com>
2024-04-13 12:58:31 -0700

    working knob1 and joy

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -6,13 +6,46 @@ enum sensorCodesEnum { JOY1,
                        KNOB4,
                        SENSORS_SIZE };
 
+struct KnobDat{
+  // clockwise == 0;
+  // counter == 1;
+  int counter = 0;
+  int direction = 0;
+  int CLK_state;
+  int prev_CLK_state;
+
+
+
+  void update(int CLK_pin, int DT_pin){
+
+    //updates counter to knob rotation
+    CLK_state = digitalRead(CLK_pin);
+    if(CLK_state != prev_CLK_state && CLK_state == HIGH){
+      if(digitalRead(DT_pin) == HIGH){
+        counter--;
+        direction = 1;
+      } else{
+        counter++;
+        direction = 0;
+      }
+    }
+    prev_CLK_state = CLK_state;
+
+    Serial.print(counter);
+    Serial.print(',');
+  }
+
+};
+
+struct KnobDat KnobDatList[4];
+
 // I KNOW I COULD DO NO '=' BUT I DONT CARE 
 enum pinCode { PIN_J1X = A0,
                PIN_J1Y = A1,
                PIN_J2X = A2,
                PIN_J2Y = A3 ,
-               PIN_K1CLK = A4,
-               PIN_K1DT = A5,
+               PIN_K1CLK = 2,
+               PIN_K1DT = 3,
                PIN_K2CLK = A6,
                PIN_K2DT = A7,
                PIN_K3CLK = A8,
@@ -24,7 +57,7 @@ enum commandsEnum { KNOB,
                     COMMANDS_SIZE };
 
 const char *commands[] = { "KNOB" };
-bool active[SENSORS_SIZE] = { true };
+bool active[SENSORS_SIZE] = {true,};
 
 void setup() {
   Serial.begin(115200);
@@ -34,7 +67,7 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // Serial.print("YO WHATS UP");
-  // Serial.print(analogRead(PIN_J1Y)+",");
+  // Serial.print(analogRead(PIN_J1Y));
   // handleJoystick1();
   loadSensorData();
 
@@ -43,9 +76,13 @@ void loop() {
 }
 
 void loadSensorData() {
-  for (short i = 0; i < SENSORS_SIZE; i++)
-    if (active[i])
+  for (short i = 0; i < SENSORS_SIZE; i++){
+    //CHANGE FOR SENSOR TOGGLE
+    active[i] = true;
+    if (active[i]){
       handleSensor(i);
+    }
+  }
 }
 
 void handleSensor(short code) {
@@ -59,19 +96,23 @@ void handleSensor(short code) {
       break;
 
     case KNOB1:
-      handleKnob1();
+      // handleKnob1();
+      KnobDatList[code - KNOB1].update(PIN_K1CLK, PIN_K1DT);
       break;
 
     case KNOB2:
-      handleKnob2();
+      // handleKnob2();
+      KnobDatList[code - KNOB1].update(PIN_K2CLK, PIN_K2DT);
       break;
 
     case KNOB3:
-      handleKnob3();
+      // handleKnob3();
+      KnobDatList[code - KNOB1].update(PIN_K3CLK, PIN_K3DT);
       break;
 
     case KNOB4:
-      handleKnob4();
+      // handleKnob4();
+      KnobDatList[code - KNOB1].update(PIN_K4CLK, PIN_K4DT);
       break;
   }
 }
@@ -85,37 +126,39 @@ void handleJoystick1() {
 }
 
 void handleJoystick2() {
-  Serial.print(analogRead(PIN_J2X)+",");
+  Serial.print(analogRead(PIN_J2X));
   Serial.print(',');
-  Serial.print(analogRead(PIN_J2Y)+",");
+  Serial.print(analogRead(PIN_J2Y));
   Serial.print(',');
 }
 
 
 void handleKnob1(){
-  Serial.print(analogRead(PIN_K1CLK)+",");
+  Serial.print(analogRead(PIN_K1CLK));
   Serial.print(',');
-  Serial.print(analogRead(PIN_K1DT)+",");
+  Serial.print(analogRead(PIN_K1DT));
   Serial.print(',');
+  
 }
 
 void handleKnob2(){
-  Serial.print(analogRead(PIN_K2CLK)+",");
+  Serial.print(analogRead(PIN_K2CLK));
   Serial.print(',');
-  Serial.print(analogRead(PIN_K2DT)+",");
+  Serial.print(analogRead(PIN_K2DT));
   Serial.print(',');
 }
 
 void handleKnob3(){
-  Serial.print(analogRead(PIN_K3CLK)+",");
+  Serial.print(analogRead(PIN_K3CLK));
   Serial.print(',');
-  Serial.print(analogRead(PIN_K3DT)+",");
+  Serial.print(analogRead(PIN_K3DT));
   Serial.print(',');
 }
 
 void handleKnob4(){
-  Serial.print(analogRead(PIN_K4CLK) +",");
+  Serial.print(analogRead(PIN_K4CLK) );
   Serial.print(',');
-  Serial.print(analogRead(PIN_K4DT) +",");
+  Serial.print(analogRead(PIN_K4DT) );
   Serial.print(',');
 }
+
