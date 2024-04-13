ede35215eeaea54bd15cb520073f72a44e08414e
GioByte10 <gvanni.bernal10@gmail.com>
2024-04-13 13:42:44 -0700

    cleaned up code

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,29 +1,46 @@
 enum sensorCodesEnum { JOY1,
-                       JOY2,
-                       KNOB1,
-                       KNOB2,
-                       KNOB3,
-                       KNOB4,
-                       SENSORS_SIZE };
+  JOY2,
+  KNOB1,
+  KNOB2,
+  KNOB3,
+  KNOB4,
+  SENSORS_SIZE 
+};
+
+enum pinCode { 
+  PIN_J1X = A0,
+  PIN_J1Y = A1,
+  PIN_J2X = A2,
+  PIN_J2Y = A3 ,
+  PIN_K1CLK = 2,
+  PIN_K1DT = 3,
+  PIN_K2CLK = A6,
+  PIN_K2DT = A7,
+  PIN_K3CLK = A8,
+  PIN_K3DT = A9,
+  PIN_K4CLK = A10,
+  PIN_K4DT = A11,
+};
+
+enum commandsEnum {
+  KNOB,
+  COMMANDS_SIZE
+};
 
 struct KnobDat{
-  // clockwise == 0;
-  // counter == 1;
   int counter = 0;
   int direction = 0;
   int CLK_state;
   int prev_CLK_state;
 
-
-
-  void update(int CLK_pin, int DT_pin){
-
-    //updates counter to knob rotation
+  void hanldeKnob(int CLK_pin, int DT_pin){
     CLK_state = digitalRead(CLK_pin);
+
     if(CLK_state != prev_CLK_state && CLK_state == HIGH){
       if(digitalRead(DT_pin) == HIGH){
         counter--;
         direction = 1;
+
       } else{
         counter++;
         direction = 0;
@@ -34,41 +51,18 @@ struct KnobDat{
     Serial.print(counter);
     Serial.print(',');
   }
-
 };
 
 struct KnobDat KnobDatList[4];
 
-// I KNOW I COULD DO NO '=' BUT I DONT CARE 
-enum pinCode { PIN_J1X = A0,
-               PIN_J1Y = A1,
-               PIN_J2X = A2,
-               PIN_J2Y = A3 ,
-               PIN_K1CLK = 2,
-               PIN_K1DT = 3,
-               PIN_K2CLK = A6,
-               PIN_K2DT = A7,
-               PIN_K3CLK = A8,
-               PIN_K3DT = A9,
-               PIN_K4CLK = A10,
-               PIN_K4DT = A11,
-               };
-enum commandsEnum { KNOB,
-                    COMMANDS_SIZE };
-
 const char *commands[] = { "KNOB" };
-bool active[SENSORS_SIZE] = {true,};
+bool active[SENSORS_SIZE] = {true};
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // Serial.print("YO WHATS UP");
-  // Serial.print(analogRead(PIN_J1Y));
-  // handleJoystick1();
   loadSensorData();
 
   //send all our data!
@@ -96,23 +90,19 @@ void handleSensor(short code) {
       break;
 
     case KNOB1:
-      // handleKnob1();
-      KnobDatList[code - KNOB1].update(PIN_K1CLK, PIN_K1DT);
+      KnobDatList[code - KNOB1].hanldeKnob(PIN_K1CLK, PIN_K1DT);
       break;
 
     case KNOB2:
-      // handleKnob2();
-      KnobDatList[code - KNOB1].update(PIN_K2CLK, PIN_K2DT);
+      KnobDatList[code - KNOB1].hanldeKnob(PIN_K2CLK, PIN_K2DT);
       break;
 
     case KNOB3:
-      // handleKnob3();
-      KnobDatList[code - KNOB1].update(PIN_K3CLK, PIN_K3DT);
+      KnobDatList[code - KNOB1].hanldeKnob(PIN_K3CLK, PIN_K3DT);
       break;
 
     case KNOB4:
-      // handleKnob4();
-      KnobDatList[code - KNOB1].update(PIN_K4CLK, PIN_K4DT);
+      KnobDatList[code - KNOB1].hanldeKnob(PIN_K4CLK, PIN_K4DT);
       break;
   }
 }
@@ -129,36 +119,6 @@ void handleJoystick2() {
   Serial.print(analogRead(PIN_J2X));
   Serial.print(',');
   Serial.print(analogRead(PIN_J2Y));
-  Serial.print(',');
-}
-
-
-void handleKnob1(){
-  Serial.print(analogRead(PIN_K1CLK));
-  Serial.print(',');
-  Serial.print(analogRead(PIN_K1DT));
-  Serial.print(',');
-  
-}
-
-void handleKnob2(){
-  Serial.print(analogRead(PIN_K2CLK));
-  Serial.print(',');
-  Serial.print(analogRead(PIN_K2DT));
-  Serial.print(',');
-}
-
-void handleKnob3(){
-  Serial.print(analogRead(PIN_K3CLK));
-  Serial.print(',');
-  Serial.print(analogRead(PIN_K3DT));
-  Serial.print(',');
-}
-
-void handleKnob4(){
-  Serial.print(analogRead(PIN_K4CLK) );
-  Serial.print(',');
-  Serial.print(analogRead(PIN_K4DT) );
   Serial.print(',');
 }
 
--- P5 JS/sketch.js
+++ P5 JS/sketch.js
@@ -1,5 +1,6 @@
 let port, reader, writer;
-let knob;
+let reactionGame;
+let previousCommand;
 
 const sensorCodesEnum = {
     JOY1: 0,
@@ -9,65 +10,111 @@ const {JOY1, JOY2} = sensorCodesEnum;
 
 
 let commandHandler = {
-  "maze": mazeHandler,
-  "knob": knobHandler
+  "MAZE": handleMaze,
+  "KNOB": handleKnob,
+  "REAC": handleReaction
 };
+
+async function connectSerial(){
+    noLoop();
+    ({ port, reader, writer } = await getPort(115200));
+    loop();
+}
 
 async function setup() {
     createCanvas(windowWidth, windowHeight);
     frameRate(60);
     angleMode(DEGREES);
 
-    noLoop();
-    ({ port, reader, writer } = await getPort(115200));
-    loop();
+    //await connectSerial();
 
-    knob = new Knob();
+    reactionGame = new ReactionGame();
+    textSize(25);
 
 }
 
-async function draw() {
-    while(true) {
-        const {value, done} = await reader.read();
 
-        if (done) {
-            reader.releaseLock();
-            break;
-        }
 
-        background(220);
+// async function draw() {
+//     while(true) {
+//         const {value, done} = await reader.read();
+//
+//         if (done) {
+//             reader.releaseLock();
+//             break;
+//         }
+//
+//         background(220);
+//
+//         let tokens = value.split(',');
+//         commandHandler[tokens[0]]();
+//
+//         circle(mouseX, mouseY, 30);
+//
+//     }
+// }
 
-        let tokens = value.split(',');
-        commandHandler[tokens[0]]();
+function draw(){
+    background(220);
 
-        circle(mouseX, mouseY, 30);
-
-    }
+    handleReaction();
 }
 
-function knobHandler(){
-    knob.display(frameCount);
+function handleKnob(){
 }
 
-function mazeHandler(){
+function handleMaze(){
     circle(100, 100, 50);
 }
 
-class Knob{
+function handleReaction(){
+    reactionGame.display();
+}
+
+class ReactionGame{
     constructor() {
-        this.theta = 0;
+        this.start = millis();
+        this.previosTime = millis();
+        this.currentTime = millis();
+        this.score = 0;
+        this.count = 0;
+        this.delta = random(500, 4000);
     }
 
-    display(theta){
-        push();
-        translate(width / 2, height / 2);
-        rotate(theta)
-        fill(0)
-        stroke(0)
-        circle(0, 0, 100);
-        stroke(255)
-        line(0, 0, 0, 50);
-        pop();
-    }
+    display(){
+        this.currentTime = millis();
+        if(this.currentTime - this.start > 3000) {
+            if (this.count < 10) {
+                text("score " + this.score.toFixed(0), 50, 50);
 
+                if (this.currentTime - this.previosTime > this.delta) {
+                    push();
+                    translate(width / 2, height / 2);
+                    fill(125, 0, 0)
+                    square(-50, -50, 100);
+                    pop();
+
+                    if (keyIsPressed && keyCode === 32) {
+                        this.score += 30000 / (this.currentTime - this.previosTime - this.delta);
+                        this.previosTime = millis();
+                        this.delta = random(500, 4000 - this.count * 400);
+                        this.count++;
+                    }
+                } else if (this.currentTime - this.previosTime >= 350 && keyIsPressed && keyCode === 32) {
+                    this.score -= 50;
+                }
+
+            } else {
+                push();
+                textAlign(CENTER);
+                textSize(70);
+                text("score " + this.score.toFixed(0), width / 2, height / 2);
+                pop();
+            }
+        }else{
+            push();
+            text()
+            pop();
+        }
+    }
 }
