be31621fcf552e380b4c74990ae1b869b2a972f6
So Hirota <hirotaso92602@gmail.com>
2024-04-13 18:47:36 -0700

    COLOR BAR WORKS (SORTA)

--- P5 JS/sketch.js
+++ P5 JS/sketch.js
@@ -33,8 +33,9 @@ function setup(){
 function draw(){
     background(220);
 
+    temp = getTemp()
     tempGraph.displayBarBase();
-    tempGraph.displaySliderBar();
+    tempGraph.displaySliderBar(temp);
 
     circle(mouseX, mouseY, 30);
 }
@@ -57,19 +58,17 @@ class BarGraph{
         pop();
     }
 
-    displaySliderBar(){
+    displaySliderBar(temp){
         push();
         translate(width / 2, height / 2);
         
-        let slider = getSliderDat(); // get the info for the slider
+        
+        let slider = this.getSliderDat(temp); // get the info for the slider
         
         strokeWeight(2);
         stroke('#222222');
         fill(slider.color);
-        // rect(this.tempBarBaseDat.x + ( (this.tempBarBaseDat.w - slider.width) / 2 ),
-        //     this.tempBarBaseDat.y + ( (this.tempBarBaseDat.h - slider.height) / 2 ),
-        //     slider.width,
-        //     slider.height);
+
         rect(this.tempBarBaseDat.x + ( (this.tempBarBaseDat.w - slider.width) / 2 ),
             this.tempBarBaseDat.y + this.tempBarBaseDat.h - 5,
             slider.width,
@@ -77,35 +76,63 @@ class BarGraph{
         
         pop();
     }
-}
 
-/**
- * packages slider info into returned object
- * NEED TO GENERATE GRADIENT BASED OFF HEAT
- */
-function getSliderDat(){
-    const width = 60;
-    const color = '#ff6a4a';
-    const height = getHeightForTemp();
-
-    return {width, height, color};
-}
-
-
-/**
- * Get height gradient
- * 
- * the range of temp is apparently 23 to 38 ish... figure out a formula
- */
-function getHeightForTemp() {
-    let result = getTemp() + 25;
-    if (result < 30){
-        result = 45;
-    } else if (result > 375){
-        result = 375;
+    getHeightForTemp(temp) {
+        // padding on top and bottom, 5 each
+        let range = this.tempBarBaseDat.h - 10;
+    
+        let result = (temp - 20) / 20 * range;
+        return result;
     }
-    return result;
+
+    getSliderDat(temp){
+        const width = 60;
+        const color = this.getColorForTemperature(temp);;
+        const height = this.getHeightForTemp(temp);
+    
+        return {width, height, color};
+    }
+
+    getColorForTemperature(temp) {
+        let hue = map(temp, 20, 40, 0, 240);
+        let saturation = 100;
+        let brightness = 100;
+        return color(hue, saturation, brightness);
+    }
+
+    
 }
+
+// /**
+//  * packages slider info into returned object
+//  * NEED TO GENERATE GRADIENT BASED OFF HEAT
+//  */
+// function getSliderDat(){
+//     const width = 60;
+//     const color = '#ff6a4a';
+//     const height = getHeightForTemp();
+
+//     return {width, height, color};
+// }
+
+
+// /**
+//  * Get height gradient
+//  * 
+//  * the range of temp is apparently 23 to 38 ish... figure out a formula
+//  */
+// function getHeightForTemp() {
+//     // padding on top and bottom, 5 each
+//     range = this.tempBarBaseDat.h - 10;
+
+//     let result = getTemp() + 25;
+//     if (result < 30){
+//         result = 45;
+//     } else if (result > 375){
+//         result = 375;
+//     }
+//     return result;
+// }
 
 /**
  * generates a random temperature
